b23a27431007fa46be41e4a3ed768513d5cf7ac9
Vaishnavi-Kyasari <vkyasari@gmail.com>
2024-10-26 14:52:05 +0530

    Add files via upload
    Initial commit

---
+++ src/main/java/com/zeotap/weather/Exception/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.zeotap.weather.Exception;
+
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+    
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Object> handleIllegalArgumentException(IllegalArgumentException ex) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("message", ex.getMessage());
+
+        return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);  // Use NOT_FOUND instead of Internal Server Error
+    }
+
+    // You can add more exception handlers here for other exceptions
+}
